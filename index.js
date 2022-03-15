61bd2e88342d5cd9e119c4e11605520ad325c1ad
Chiragchachada <84634907+Chiragchachada@users.noreply.github.com>
2022-03-15 11:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,138 +1,24 @@
-<div className=" flex   justify-center  via-red-500 to-pink-500 py-16 pb-40 ">
-        <div className="  border-2 p-8 rounded-xl flex justify-center w-1/3 bg-gray-300 space-y-8">
-           <div className="w-80 ">
-           <div>
+<div className="main justify-center  ">
+              <div class="section-content flex  ">
+
+
+              <div class="central-content p-40 w-3/6  ">
+                <div class="font-semibold text-5xl">The Best Logistics Service Provider</div>
+                <div  class="mt-3.5 text-2xl font-semibold">We are flexible and delicated Logistics Service Partner. We work with you to design custom solutions that add value to your business </div>
+                <div class="button-section border-4 rounded-3xl w-40 p-2   mt-4 	ml-32">
+                    <button class="button text-xl font-black font-bold	" onClick={()=>goToService()}>Our Services</button>
+                </div>
+
+            </div>
+
+
+            <div class="w-1/2 flex justify-center m-8 ">
+             <img  src="https://res.cloudinary.com/dcqew2kqy/image/upload/v1637749610/20945966_j7it1g.jpg" alt="group-chat" class=" m-4 h-2/3 mt-20" />
+            </div>
+
            
-              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create your account</h2>
+        </div>
 
-           </div >
-
-           <br/>
-
-            <form >
-
-            <div className="rounded-md shadow-sm -space-y-px">
-
-            <div>
-                <label className="sr-only">
-                  Username
-                </label>
-                <input
-                  id="Username"
-                  name="Username"
-                  type="text"
-                  value={userName}
-                  onChange={(e)=>updateUserName(e.target.value)}
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Username"
-                />
-              </div>
-
-              <br/>
-              
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  value={emailId}
-                  required
-                  onChange={(e)=>updateEmailId(e.target.value)}
-                  autoComplete="email"
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
-                />
-              </div>
-
-              <br/>
-
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-
-                  autoComplete="current-password"
-                  value={password}
-                  onChange={(e)=>updatePassword(e.target.value)}
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
-                />
-              </div>
-
-              <br/>
-
-              <div>
-                <label htmlFor="password" className="sr-only">
-                 Re enter Password
-                </label>
-                <input
-                  id="Renter-password"
-                  name="Renter-pass"
-                  type="password"
-                  autoComplete="re-enter-password"
-                  value={confirmPassword}
-                  onChange={(e)=>updateConfirmPassword(e.target.value)}
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Re Enter Password"
-                />
-              </div>
-
-              <br/>
-
-              <div>
-                <label htmlFor="password" className="sr-only">
-                 Phone Number
-                </label>
-                <input
-                  id="phone"
-                  name="telephone"
-                  type="tel"
-                  value={phone}
-                  onChange={(e)=>updatePhone(e.target.value)}
-                  pattern="[0-9]{10}"
-                  required
-                  className="  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:border-indigo-500  sm:text-sm"
-                  placeholder="Number"
-                />
-              </div>
-
-            </div>
-
-            <br/>
-
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                 onClick={(e)=>createUserAccount(e)}
-              >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <span class="material-icons">lock</span>
-                </span>
-               Register
-              </button>
-            </div>
-
-            
-
-              
-
-
-
-            </form>
-
-            </div>
-
+        
 
         </div>
-     </div>
